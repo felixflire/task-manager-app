9340cbfe7eb4a5c432ab0f1727743c28f7db63bb
Felix <felixflire3@gmail.com>
2026-05-17 03:24:03 +0300

    add homepage

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'taskmanager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'taskmanager', 'templates')], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include,path
 
+from . import views
+
 urlpatterns = [
+    path("", views.home, name = "home"),
     path('admin/', admin.site.urls),
     path("tasksapp/", include("tasksapp.urls")),
     path("users/", include("users.urls"))
